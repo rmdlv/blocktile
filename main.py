1d74220b3b03999b4f80f3ec6ac69238fe10d9e8
Vladimir <60815271+rmdlv@users.noreply.github.com>
2021-11-26 18:35:47 +0800

    zoom fix

--- main.py
+++ main.py
@@ -5,25 +5,19 @@ from io import BytesIO
 
 from mcbiomes import genLayer as gl
 
+from PIL.Image import BOX
+
 app = FastAPI()
 
 
 @app.get("/{seed}/{zoom}/{x}/{z}.png")
 def tile(seed: int, zoom: int, x: int, z: int):
     layer = gl.genlayer(seed)
-    if zoom > 16:
-        zoom = 16
+    zoom = 1 if zoom < 1 else zoom
+    zoom = 16 if zoom > 16 else zoom
     scale = 16 * zoom
-    image = gl.getImage(
-        layer.getInts(
-            int(x / scale) * scale,
-            int(z / scale) * scale,
-            scale,
-            scale,
-        ),
-        scale,
-        scale,
-    )
+    chunk = layer.getInts(int(x / scale) * scale, int(z / scale) * scale, scale, scale)
+    image = gl.getImage(chunk, scale, scale).resize((256, 256), BOX)
     buffer = BytesIO()
     image.save(buffer, "png")
     return StreamingResponse(BytesIO(buffer.getvalue()))
