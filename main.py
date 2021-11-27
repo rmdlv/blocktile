efb1344b7bb67a34780758096fc017782f018f1b
Vladimir <60815271+rmdlv@users.noreply.github.com>
2021-11-27 15:15:13 +0800

    revert

--- main.py
+++ main.py
@@ -14,13 +14,8 @@ app = FastAPI()
 async def tile(seed: int, zoom: int, x: int, z: int):
     layer = gl.genlayer(seed)
     zoom = 0 if zoom < 0 else zoom
-<<<<<<< HEAD
-    zoom = 2 if zoom > 2 else zoom
-    scale = 16 * (16 - zoom * (10 - zoom * 2))
-=======
     zoom = 15 if zoom > 15 else zoom
     scale = 16 * (16 - zoom)
->>>>>>> parent of a395ccd (last zoom fix)
     chunk = layer.getInts(x * scale, z * scale, scale, scale)
     image = gl.getImage(chunk, scale, scale).resize((256, 256), BOX)
     buffer = BytesIO()
